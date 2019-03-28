d82e6f27d06e51a552aa03b52d8747864d5c2ed8
Alan Dipert <alan@dipert.org>
2019-03-28 11:45:02 -0700

    Add app.R and assorted JS files to .Rbuildignore in scaffolded projects

--- R/scaffold_input.R
+++ R/scaffold_input.R
@@ -68,7 +68,7 @@ scaffoldReactShinyInput <- function(name, npmPkgs = NULL, edit = interactive()) 
     )
   )
 
-  usethis::use_build_ignore(c("node_modules", "srcjs"))
+  usethis::use_build_ignore(c("node_modules", "srcjs", "app.R", "package.json", "webpack.config.js", "yarn.lock"))
   usethis::use_git_ignore(c("node_modules"))
   lapply(c("htmltools", "shiny", "reactR"), usethis::use_package)
 
