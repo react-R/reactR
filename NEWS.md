3f68a4c1b738ce9452fba8ad976ce020422ae834
Jonathan Carroll <github@jcarroll.com.au>
2022-10-19 22:58:59 +1030

    update react to 18.2.0

--- NEWS.md
+++ NEWS.md
@@ -1,3 +1,7 @@
+# reactR 0.5
+
+* Update react to 18.2.0
+
 # reactR 0.4.4
 
 * Update react to `16.12.0`
--- R/meta.R
+++ R/meta.R
@@ -1,3 +1,3 @@
 #'@keywords internal
-react_version <- function(){'17.0.0'}
+react_version <- function(){'18.2.0'}
 babel_version <- function(){'6.26.0'}
